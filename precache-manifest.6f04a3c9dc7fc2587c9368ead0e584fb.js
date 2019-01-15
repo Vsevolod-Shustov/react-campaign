4df4d6ca14303a9eb3902e66c51b0abf079ed3c9
Vsevolod-Shustov <vsevolod.shustov@gmail.com>
2019-01-15 12:20:26 +0300

    Updates

---
+++ precache-manifest.6f04a3c9dc7fc2587c9368ead0e584fb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "c89a4a0400c20b26313dd83b698629dd",
+    "url": "/static/media/swamp.c89a4a04.svg"
+  },
+  {
+    "revision": "4105161ab53067f5550e6f936ea71757",
+    "url": "/static/media/plains.4105161a.svg"
+  },
+  {
+    "revision": "b4cf49cfcbcedee4381d7862b3d1ea20",
+    "url": "/static/media/mountain.b4cf49cf.svg"
+  },
+  {
+    "revision": "a91d9dd66c06d04236771259d980a941",
+    "url": "/static/media/hills.a91d9dd6.svg"
+  },
+  {
+    "revision": "17188498d5d612107551421d82cfd061",
+    "url": "/static/media/forest.17188498.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d0b86892093feab09a16",
+    "url": "/static/js/main.d0b86892.chunk.js"
+  },
+  {
+    "revision": "93b8bcc8757aea912f43",
+    "url": "/static/js/1.93b8bcc8.chunk.js"
+  },
+  {
+    "revision": "d0b86892093feab09a16",
+    "url": "/static/css/main.43ead89c.chunk.css"
+  },
+  {
+    "revision": "7bc657f2867990af3bbadc83f90a6d00",
+    "url": "/index.html"
+  }
+];
