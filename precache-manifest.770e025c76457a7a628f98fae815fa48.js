56b93eba4f4549b997a18d0627cddca6aa29d6b3
Vsevolod-Shustov <vsevolod.shustov@gmail.com>
2019-01-15 12:27:13 +0300

    Updates

---
+++ precache-manifest.770e025c76457a7a628f98fae815fa48.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "c89a4a0400c20b26313dd83b698629dd",
+    "url": "/react-campaign/static/media/swamp.c89a4a04.svg"
+  },
+  {
+    "revision": "4105161ab53067f5550e6f936ea71757",
+    "url": "/react-campaign/static/media/plains.4105161a.svg"
+  },
+  {
+    "revision": "b4cf49cfcbcedee4381d7862b3d1ea20",
+    "url": "/react-campaign/static/media/mountain.b4cf49cf.svg"
+  },
+  {
+    "revision": "a91d9dd66c06d04236771259d980a941",
+    "url": "/react-campaign/static/media/hills.a91d9dd6.svg"
+  },
+  {
+    "revision": "17188498d5d612107551421d82cfd061",
+    "url": "/react-campaign/static/media/forest.17188498.svg"
+  },
+  {
+    "revision": "9093183b6d62fba7355e",
+    "url": "/react-campaign/static/js/runtime~main.9093183b.js"
+  },
+  {
+    "revision": "e51ee4c1a3657bf61a20",
+    "url": "/react-campaign/static/js/main.e51ee4c1.chunk.js"
+  },
+  {
+    "revision": "93b8bcc8757aea912f43",
+    "url": "/react-campaign/static/js/1.93b8bcc8.chunk.js"
+  },
+  {
+    "revision": "e51ee4c1a3657bf61a20",
+    "url": "/react-campaign/static/css/main.43ead89c.chunk.css"
+  },
+  {
+    "revision": "3cc5eea77947dcc4b487f750172d16ab",
+    "url": "/react-campaign/index.html"
+  }
+];
